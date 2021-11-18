94a9bdbf6d181a271e1ce953e277d98c92596438
Samuel Felipe <samuelfelipef@gmail.com>
2021-11-17 23:16:02 -0300

    create static layout of signup page

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,18 @@
+import * as S from '../../styles/SignUpAndSignInStyle';
+
 const SignUp = () => {
   return (
-    <h1>SignUp</h1>
+    <S.PageStyle>
+      <S.Text>Bem vindo ao GratiBox</S.Text>
+      <S.Form>
+        <S.Input placeholder="Nome" autoFocus />
+        <S.Input placeholder="Email" autoFocus />
+        <S.Input placeholder="Senha" autoFocus />
+        <S.Input placeholder="Confirmar Senha" autoFocus />
+      </S.Form>
+      <S.Button>Cadastrar</S.Button>
+    </S.PageStyle>
   );
-}
+};
 
 export default SignUp;
---
+++ src/styles/SignUpAndSignInStyle.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+const PageStyle = styled.div`
+  height: 100vh;
+  background-color: #6d7ce4;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Text = styled.h1`
+  font-size: 28px;
+  font-weight: 700;
+  color: #ffffff;
+  margin-top: 101px;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const Form = styled.form`
+  margin-top: 43px;
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
+const Input = styled.input`
+  width: calc(100vw - 100px);
+  height: 64px;
+  border: 1px solid #604848;
+  background: #ffffff;
+  border-radius: 10px;
+  padding-left: 22px;
+  border: 1px solid #604848;
+  font-size: 22px;
+
+  &::placeholder {
+    font-family: 'Roboto', sans-serif;
+    font-size: 22px;
+    font-weight: 500;
+    color: rgba(96, 72, 72, 0.4);
+  }
+`;
+
+const Button = styled.button`
+  margin-top: 52px;
+  background-color: #8c97ea;
+  border-radius: 10px;
+  border: 0;
+  width: 237px;
+  height: 56px;
+  font-size: 36px;
+  font-weight: 700;
+  color: #ffffff;
+  font-family: 'Roboto', sans-serif;
+`;
+
+export { PageStyle, Text, Form, Input, Button };
