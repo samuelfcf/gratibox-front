e2dd30ac07a501e863e421f137f965cacb5f7103
Samuel Felipe <samuelfelipef@gmail.com>
2021-11-17 22:33:07 -0300

    create Home style

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,19 @@
+import HomeImage from '../../assets/image05.webp';
+import * as S from './style';
+
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <S.PageStyle>
+      <S.PageBigText>Bem vindo ao Gratibox</S.PageBigText>
+      <S.PageSmallText>
+        Receba em casa um box com chás, produtos orgânicos, incensos e muito
+        mais...
+      </S.PageSmallText>
+      <S.Img src={HomeImage} alt="home_img" />
+      <S.ButtonSignUp>Quero começar</S.ButtonSignUp>
+      <S.ButtonSignIn>Já sou grato</S.ButtonSignIn>
+    </S.PageStyle>
   );
-}
+};
 
 export default Home;
---
+++ src/pages/Home/style.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+const PageStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #6d7ce4;
+  height: 100vh;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 482px;
+  position: relative;
+`;
+
+const PageBigText = styled.h1`
+  font-family: Roboto, sans-serif;
+  font-weight: 700;
+  font-size: 28px;
+  margin-top: 53px;
+  color: #ffffff;
+`;
+
+const PageSmallText = styled.h3`
+  font-family: Roboto, sans-serif;
+  font-weight: 300;
+  font-size: 18px;
+  text-align: center;
+  color: #ffffff;
+  margin: 45px 40px 0px 40px;
+`;
+
+const ButtonSignUp = styled.button`
+  position: absolute;
+  bottom: 42px;
+  width: 202px;
+  height: 45px;
+  border: 0;
+  border-radius: 10px;
+  background-color: #8c97ea;
+  font-family: Roboto, sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  color: #ffffff;
+`;
+
+const ButtonSignIn = styled.button`
+  position: absolute;
+  bottom: 8px;
+  border: 0;
+  background: none;
+  font-size: 18px;
+  color: #ffffff;
+  font-weight: 700;
+`;
+
+export {
+  PageStyle,
+  Img,
+  PageBigText,
+  PageSmallText,
+  ButtonSignIn,
+  ButtonSignUp,
+};
