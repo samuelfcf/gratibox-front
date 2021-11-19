8fb52b7d244544715fe069d5b140b61e4112344d
Samuel Felipe <samuelfelipef@gmail.com>
2021-11-19 19:05:57 -0300

    refact sign in page and create user context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { useState } from 'react';
 import Pages from './Pages';
 import GlobalStyle from './styles/GlobalStyle';
+import UserContext from './contexts/UserContext';
 
 const App = () => {
+  const userStorage = JSON.parse(localStorage.getItem('@user'));
+  const [user, setUser] = useState(userStorage);
+
   return (
-    <Router>
-      <GlobalStyle />
-      <Pages />
-    </Router>
-    );
-}
+    <UserContext.Provider value={{ user, setUser }}>
+      <Router>
+        <GlobalStyle />
+        <Pages />
+      </Router>
+    </UserContext.Provider>
+  );
+};
 
 export default App;
